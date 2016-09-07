a893a3d1d759a4dce420078d8f4cbb74ebde6568
Agomeza <Agomeza@Agomeza-PC.bitconsulting.local>
2016-09-07 10:23:47 -0400

    Prueba de commit

--- SpringMVC/src/main/java/com/bpago/spring/HomeController.java
+++ SpringMVC/src/main/java/com/bpago/spring/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("prueba", "Hellooooooooooooo");
+		model.addAttribute("prueba", "Hola  xD, Prueba Spring MVC");
 		
 		return "home";
 	}
